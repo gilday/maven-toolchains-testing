55707d3db2c5ac33baead6947053e300841d308d
Johnathan Gilday <me@johnathangilday.com>
2021-06-10 22:46:57 -0400

    :tada: Initial Commit

---
+++ src/test/java/com/github/gilday/AppIT.java
@@ -0,0 +1,13 @@
+package com.github.gilday;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/** Integration test for simple App. */
+final class AppIT {
+  @Test
+  public void some_test() {
+    assertTrue(true);
+  }
+}
